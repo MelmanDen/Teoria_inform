67c608b6c623ffa6c312a9d9f0fec6536350c154
MelmanDen <zzz-danil-zzz@mail.ru>
2022-03-23 18:42:52 +0300

    Исправил ошибочный вывод (блочного кодирование) на сверточное кодирование

--- Encoding_svertochniy_code(2laba).py
+++ Encoding_svertochniy_code(2laba).py
@@ -1,36 +1,36 @@
 # import tkinter as tk
 # import tkinter.messagebox as mb
 
-primal_text = "abcде"
-summators = []
+primal_text = "1011"
+# summators = []
 spisok_text_to_bit = []
-register = [0, 0, 0]
 abc = []
-# замена на знаки вопрос элементы которые не подхожят под ACSII
-primal_text = primal_text.encode(encoding='ascii', errors='replace')
-primal_text = primal_text.decode('ascii', 'replace')
+encoded_string_finished = ''
 print(primal_text, type(primal_text))
 for i in primal_text:
-    spisok_text_to_bit.append(bin(ord(i))[2:].zfill(8))
+    spisok_text_to_bit.append(bin(ord(i))[2:])
+print(spisok_text_to_bit)
+spisok_text_to_bit = "".join(spisok_text_to_bit)
 print(spisok_text_to_bit)
 
-# вводим сумматоры Доделать!!!!
-kol_summators = int(input("введите колво сумматоров "))
-# сдлеать вывод месседжбоксов для ввода сумматоров
-for i in range(kol_summators):
-    # сделать ошибку ввода для чисел больше количества регистров!!!
-    summators.append(list(input('введите сумматоры ')))
 
-for i in range(len(summators)):
-    for j in range(len(summators[i])):
-        summators[i][j] = int(summators[i][j])
-print(summators, type(summators[0][0]))
+# # вводим сумматоры Доделать!!!!
+# kol_summators = int(input("введите колво сумматоров "))
+# # сдлеать вывод месседжбоксов для ввода сумматоров
+# for i in range(kol_summators):
+#     # сделать ошибку ввода для чисел больше количества регистров!!!
+#     summators.append(list(input('введите сумматоры ')))
+#
+# for i in range(len(summators)):
+#     for j in range(len(summators[i])):
+#         summators[i][j] = int(summators[i][j])
+# print(summators, type(summators[0][0]))
 
 def encoding(spisok_text_to_bit):
     # вводим элементы которые потребуются
-    summators = [[0, 1], [1, 2], [0, 1, 2]]
+    summators = [[0, 1], [1, 2]]
     spisok_polinomov = []
-    k = []
+    spisok_polinov_do_sumpomod2 = []
     spisok_indeksov_edinic = []
 
     # Из списка битов вытаскиваем индексы единиц для кодирования полиномами
@@ -46,14 +46,14 @@ def encoding(spisok_text_to_bit):
         for j in range(len(summators[i])):
             for m in range(len(spisok_indeksov_edinic)):
                 spisok_polinomov.append(spisok_indeksov_edinic[m] + summators[i][j])
-        k.append(spisok_polinomov)
+        spisok_polinov_do_sumpomod2.append(spisok_polinomov)
         spisok_polinomov = []
 
     # сложение по модулю 2 полиномов
-    for i in range(len(k)):
+    for i in range(len(spisok_polinov_do_sumpomod2)):
         f = []
-        for j in k[i]:
-            if (k[i].count(j) % 2) != 0:
+        for j in spisok_polinov_do_sumpomod2[i]:
+            if (spisok_polinov_do_sumpomod2[i].count(j) % 2) != 0:
                 f.append(j)
         f = list(set(f))
         spisok_polinomov.append(f)
@@ -77,17 +77,15 @@ def encoding(spisok_text_to_bit):
         i += 1
         encoded_string.append(encoded_list)
     # Собираем в красивую конструкцию и выводим из под функции
-    global asd
-    asd = ''
+    global encoded_string_finished
+    encoded_string_finished = ''
     print(encoded_string)
-    for j in range(len(encoded_string) - 1):
-        asd += ''.join(encoded_string[j]) + '.' + ''.join(encoded_string[j + 1]) + '.'
-    print(asd)
-    return asd
+    for j in range(len(encoded_string)):
+        encoded_string_finished += ''.join(encoded_string[j]) + '.'
+    return encoded_string_finished
 
 
 # каждый символ заносим в функцию возращая список закодированных символов
-for i in range(len(spisok_text_to_bit)):
-    encoding(spisok_text_to_bit[i])
-    abc.append(asd)
-print(abc)
+
+encoding(spisok_text_to_bit)
+print(encoded_string_finished)
