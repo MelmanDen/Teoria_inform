a4fd51fd6c9ba47160244b8cb2ad376d0bb07c34
MelmanDen <zzz-danil-zzz@mail.ru>
2022-03-28 22:03:17 +0300

    Сделал декодирование и перевод строки битов обратно в строку текста

--- Encoding_svertochniy_code(2laba).py
+++ Encoding_svertochniy_code(2laba).py
@@ -1,17 +1,19 @@
 # import tkinter as tk
 # import tkinter.messagebox as mb
+import binascii
 
-primal_text = "1011"
+primal_text = "asd"
 # summators = []
-spisok_text_to_bit = []
 abc = []
 encoded_string_finished = ''
-print(primal_text, type(primal_text))
-for i in primal_text:
-    spisok_text_to_bit.append(bin(ord(i))[2:])
+
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+spisok_text_to_bit = text_to_bits(primal_text)
 print(spisok_text_to_bit)
-spisok_text_to_bit = "".join(spisok_text_to_bit)
-print(spisok_text_to_bit)
+
 
 
 # # вводим сумматоры Доделать!!!!
@@ -28,7 +30,7 @@ print(spisok_text_to_bit)
 
 def encoding(spisok_text_to_bit):
     # вводим элементы которые потребуются
-    summators = [[0, 1], [1, 2]]
+    summators = [[0, 1], [0, 2]]
     spisok_polinomov = []
     spisok_polinov_do_sumpomod2 = []
     spisok_indeksov_edinic = []
@@ -88,4 +90,69 @@ def encoding(spisok_text_to_bit):
 # каждый символ заносим в функцию возращая список закодированных символов
 
 encoding(spisok_text_to_bit)
-print(encoded_string_finished)
+print(encoded_string_finished, type(encoded_string_finished))
+encoded_string_finished = encoded_string_finished[:-1]
+encoded_string_finished = encoded_string_finished.split('.')
+print(encoded_string_finished, type(encoded_string_finished))
+
+def decoding(encoded_string_finished):
+    summators = [[0, 1], [0, 2]]
+    global registrs
+    registrs = []
+    kol_registrov = 0
+    global decoded_string
+    decoded_string = ''
+
+    for i in summators:
+        if kol_registrov < max(i):
+            kol_registrov = max(i)
+
+    for i in range(kol_registrov+1):
+        registrs.append(0)
+    print(registrs)
+
+    def append_zero():
+        for i in reversed(range(len(registrs))):
+            registrs[i] = registrs[i-1]
+        registrs[0] = 0
+        return registrs
+
+    def calc_prov_bits():
+        global proverochnie_bits
+        proverochnie_bits = ''
+        for j in range(len(summators)):
+            c = 0
+            for m in range(len(summators[j])):
+                c += registrs[summators[j][m]]
+            if c % 2 == 1:
+                proverochnie_bits += ''.join('1')
+            elif c % 2 == 0:
+                proverochnie_bits += ''.join('0')
+        return proverochnie_bits
+
+    for i in range(len(encoded_string_finished)):
+        append_zero()
+        calc_prov_bits()
+        if proverochnie_bits != encoded_string_finished[i]:
+            registrs[0] = 1
+            decoded_string += ''.join('1')
+        elif proverochnie_bits == encoded_string_finished[i]:
+            decoded_string += ''.join('0')
+
+    print(decoded_string)
+
+    def text_from_bits(binstring, encoding='utf-8', errors='surrogatepass'):
+        n = int(binstring, 2)
+        return int2bytes(n).decode(encoding, errors)
+
+    def int2bytes(i):
+        hex_string = '%x' % i
+        n = len(hex_string)
+        return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+
+    decoded_primal_text = text_from_bits(decoded_string)
+    print(decoded_primal_text)
+
+    return decoded_primal_text
+decoding(encoded_string_finished)
+print(decoding(encoded_string_finished), "END")
